31ab90f21f53b8ee00bca0a625698dafa6db2272
Henri Koci <74716541+hkoci@users.noreply.github.com>
2021-12-20 11:19:44 +0000

    🚧[Backend] Spring Boot Initializer

---
+++ src/backend/blockchainIOT/src/main/java/uk/ac/burnel/blockchainIOT/BlockchainIotApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.burnel.blockchainIOT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainIotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockchainIotApplication.class, args);
+	}
+
+}
---
+++ src/backend/blockchainIOT/src/test/java/uk/ac/burnel/blockchainIOT/BlockchainIotApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.burnel.blockchainIOT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockchainIotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
