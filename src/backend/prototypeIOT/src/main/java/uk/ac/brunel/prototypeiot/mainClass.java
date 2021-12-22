4201d81939d58a60ee08e00a42a836f8613cff5c
Henri Koci <74716541+hkoci@users.noreply.github.com>
2021-12-22 14:11:13 +0000

    Hello World CI (Test 2)

--- src/backend/prototypeIOT/src/main/java/uk/ac/brunel/prototypeiot/mainClass.java
+++ src/backend/prototypeIOT/src/main/java/uk/ac/brunel/prototypeiot/mainClass.java
@@ -15,7 +15,7 @@ public class mainClass {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello World");
+        System.out.println("Hello World - Test 2");
     }
     
 }
