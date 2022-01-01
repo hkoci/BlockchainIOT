04e3c454ccdb07bd04a536fc2ff662fc84cdae61
Henri Koci <74716541+hkoci@users.noreply.github.com>
2022-01-01 16:21:30 +0000

    🧪[DevOps] Cleanup un-used import

--- src/backend/prototypeIOT/src/test/java/uk/ac/brunel/prototypeiot/iotClientTest.java
+++ src/backend/prototypeIOT/src/test/java/uk/ac/brunel/prototypeiot/iotClientTest.java
@@ -14,8 +14,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import uk.ac.brunel.prototypeiot.iotServer;
-
 /**
  *
  * @author hkoci
