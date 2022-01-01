1f62a905dbb57d443f3cf0bd54a71422ec9bfa4d
Henri Koci <74716541+hkoci@users.noreply.github.com>
2022-01-01 14:30:28 +0000

    🚧[Network] Created server send/receive methods

--- src/backend/prototypeIOT/src/main/java/uk/ac/brunel/prototypeiot/iotServer.java
+++ src/backend/prototypeIOT/src/main/java/uk/ac/brunel/prototypeiot/iotServer.java
@@ -15,8 +15,20 @@ import java.net.*;
 public class iotServer {
 
     public static void main(String[] args) throws IOException {
-        //Create server socket with port 4999
-        ServerSocket serverSockObject = new ServerSocket(4999);
+
+        //Recieve client message, starting server argument at port 4999
+        String message = receiveMessage(4999);
+
+        //Print message recieved from client
+        System.out.println("Client says: " + message);
+        
+        //Send message to client, starting server argument at port 4999 and with message arg.
+        sendMessage(4999, "Hello Client - I see you!");
+    }
+
+    public static String receiveMessage(int portNumber) throws IOException{
+        //Create server socket with port specified port parameter
+        ServerSocket serverSockObject = new ServerSocket(portNumber);
         Socket socketObject = serverSockObject.accept();
         
         //Socket accepted by client - display successful connection
@@ -25,18 +37,29 @@ public class iotServer {
         //Create new reader objects for input/output
         InputStreamReader inputObject = new InputStreamReader(socketObject.getInputStream());
         BufferedReader bufferObject = new BufferedReader(inputObject);
+
+        //Close the socket session when the message has been recived
+        serverSockObject.close();
+
+        //Return the client output from bufferedreader object
+        return bufferObject.readLine();
+    }
+
+    public static void sendMessage(int portNumber, String message) throws IOException{
+        //Create server socket with port 4999
+        ServerSocket serverSockObject = new ServerSocket(portNumber);
+        Socket socketObject = serverSockObject.accept();
         
-        //Print the client output from bufferedreader object
-        System.out.println("Client says: " + bufferObject.readLine());
+        //Socket accepted by client - display successful connection
+        System.out.println("Socket connected to client!");
         
         //Send a message to the client using the PrintWriter
         PrintWriter printObject = new PrintWriter(socketObject.getOutputStream());
-        printObject.println("Hello Client - I see you!");
+        printObject.println(message);
         printObject.flush();
 
         //Close the socket session when the message has been sent over
         serverSockObject.close();
-        
     }
     
 }
