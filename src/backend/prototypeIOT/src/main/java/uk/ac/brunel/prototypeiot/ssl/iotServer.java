03a6e8b968035ba479f2533e38e07461f8399af0
Henri Koci <74716541+hkoci@users.noreply.github.com>
2022-01-13 11:54:58 +0000

    🔒[Network] Refactor server code using SSLServerSocketFactory

--- src/backend/prototypeIOT/src/main/java/uk/ac/brunel/prototypeiot/ssl/iotServer.java
+++ src/backend/prototypeIOT/src/main/java/uk/ac/brunel/prototypeiot/ssl/iotServer.java
@@ -4,6 +4,7 @@
  */
 package uk.ac.brunel.prototypeiot.ssl;
 
+import javax.net.ssl.*;
 import javax.net.*; 
 import java.net.*;
 import java.io.*;
@@ -52,6 +53,61 @@ public class iotServer {
      */
     public static void main(String[] args) throws IOException {
         System.out.println("Port" + serverPort);
+        
+        //Recieve client message, starting server argument at defined port
+        String message = receiveMessage(serverPort);
+
+        //Print message recieved from client
+        System.out.println("Client says: " + message);
+        
+        //Send message to client, starting server argument at defined port with message
+        sendMessage(serverPort, "Hello Client - I see you!");
+    }
+    
+    public static String receiveMessage(int portNumber) throws IOException{
+        //Instantiate SSLSocketFactory to create ServerSocket with SSL comms.
+        SSLServerSocketFactory socketFactory = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+        
+        //Create Socket object at defined port number
+        SSLServerSocket serverSockObject =  (SSLServerSocket) socketFactory.createServerSocket(portNumber);
+        
+        //Launch server socket with port specified port parameter
+        Socket socketObject = serverSockObject.accept();
+        
+        //Socket accepted by client - display successful connection
+        System.out.println("Socket connected to client!");
+        
+        //Create new reader objects for input/output
+        InputStreamReader inputObject = new InputStreamReader(socketObject.getInputStream());
+        BufferedReader bufferObject = new BufferedReader(inputObject);
+
+        //Close the socket session when the message has been recived
+        serverSockObject.close();
+
+        //Return the client output from bufferedreader object
+        return bufferObject.readLine();
+    }
+
+    public static void sendMessage(int portNumber, String message) throws IOException{
+        //Instantiate SSLSocketFactory to create ServerSocket with SSL comms.
+        SSLServerSocketFactory socketFactory = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+        
+        //Create Socket object at defined port number
+        SSLServerSocket serverSockObject =  (SSLServerSocket) socketFactory.createServerSocket(portNumber);
+        
+        //Launch server socket with port specified port parameter
+        Socket socketObject = serverSockObject.accept();
+        
+        //Socket accepted by client - display successful connection
+        System.out.println("Socket connected to client!");
+        
+        //Send a message to the client using the PrintWriter
+        PrintWriter printObject = new PrintWriter(socketObject.getOutputStream());
+        printObject.println(message);
+        printObject.flush();
+
+        //Close the socket session when the message has been sent over
+        serverSockObject.close();
     }
     
 }
