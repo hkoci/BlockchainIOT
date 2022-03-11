b58a33b797c601a0535bd6709cfa4d93776f8366
Henri Koci <74716541+hkoci@users.noreply.github.com>
2022-03-11 10:57:55 +0000

    🪲 [Deprecated] IOTA store docs and compiled binaries
    Last revised version of IOTA code before changing implementation (!)

--- src/backend/prototypeIOT/src/main/java/uk/ac/brunel/prototypeiot/ssl/mainLoader.java
+++ src/backend/prototypeIOT/src/main/java/uk/ac/brunel/prototypeiot/ssl/mainLoader.java
@@ -4,6 +4,12 @@
  */
 package uk.ac.brunel.prototypeiot.ssl;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+
 /**
  *
  * @author Henri
@@ -13,8 +19,18 @@ public class mainLoader {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+        SSLSocketFactory sslsocketfactory = SSLSocketFactory.getDefault();
+        SSLSocket sslsocket = (SSLSocket) sslsocketfactory.createSocket("localhost", 4999);
+        InputStream in = sslsocket.getInputStream();
+        OutputStream out = sslsocket.getOutputStream();
+
+        out.write(1);
+        while (in.available() > 0) {
+            System.out.print(in.read());
+        }
+
+        System.out.println("Secured connection performed successfully");
     }
     
 }
