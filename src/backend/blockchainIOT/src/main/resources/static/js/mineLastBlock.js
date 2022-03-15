ac72adfe613c3eeebd75482eb93810e21b993fa8
Henri Koci <74716541+hkoci@users.noreply.github.com>
2022-03-15 20:09:38 +0000

    🌐[Frontend] Last Block Miner implementation

--- src/backend/blockchainIOT/src/main/resources/static/js/mineLastBlock.js
+++ src/backend/blockchainIOT/src/main/resources/static/js/mineLastBlock.js
@@ -3,4 +3,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
  */
 
-
+$(document).ready( function () {
+    $('#mineBtn').click(function(){
+        //Hide Modal
+        $('#mineModal').modal('hide');
+        //Show progress bar modal anim
+        $('#minerLoaderModal').modal('show'); 
+        //Perform mining request to endpoint
+        $.get( "api/fog/block/mine", function( data ) {
+          $( ".result" ).html( data );
+          //Remove progress indicator
+          $('#minerLoaderModal').modal('hide');
+          //Reload cards
+          location.reload();
+        });
+    });
+});
