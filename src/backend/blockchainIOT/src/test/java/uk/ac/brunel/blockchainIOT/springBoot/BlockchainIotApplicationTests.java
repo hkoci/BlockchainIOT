3fcb442999e828047d81b2de9acb942241267b50
Henri Koci <74716541+hkoci@users.noreply.github.com>
2022-03-15 10:02:50 +0000

    🧪[CI] Initialise Spring Tests CI

---
+++ src/backend/blockchainIOT/src/test/java/uk/ac/brunel/blockchainIOT/springBoot/BlockchainIotApplicationTests.java
@@ -0,0 +1,21 @@
+package uk.ac.brunel.blockchainIOT.springBoot;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class BlockchainIotApplicationTests {
+    
+	@Autowired
+	private BlockchainIotController restController;
+
+	@Test
+	void contextLoads() throws Exception {
+            assertThat(restController).isNotNull();
+	}
+
+}
