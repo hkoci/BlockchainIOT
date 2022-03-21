ea48f37271256922dfc8d6ca452b666b5cc00e1f
Henri Koci <74716541+hkoci@users.noreply.github.com>
2022-03-21 09:47:10 +0000

    🚧[Network] Run IOT Server code on Spring runtime

--- src/backend/blockchainIOT/src/main/java/uk/ac/brunel/blockchainIOT/springBoot/springApp.java
+++ src/backend/blockchainIOT/src/main/java/uk/ac/brunel/blockchainIOT/springBoot/springApp.java
@@ -1,12 +1,26 @@
 package uk.ac.brunel.blockchainIOT.springBoot;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import uk.ac.brunel.blockchainIOT.edgeClient.iotServer;
 
 @SpringBootApplication
 public class springApp {
 
     public static void main(String[] args) {
         SpringApplication.run(springApp.class, args);
+        
+        //Run the server continuously.
+
+            try {
+                iotServer.main(args);
+            } catch (IOException ex) {
+                Logger.getLogger(springApp.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
     }
 }
